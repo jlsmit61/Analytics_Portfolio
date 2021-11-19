89f92ff56c195690bb465c5ea9759751ea9526b7
jlsmit61 <60348438+jlsmit61@users.noreply.github.com>
2021-11-18 21:06:53 -0500

    Update Basic_R.R

--- Basic_R.R
+++ Basic_R.R
@@ -1,5 +1,4 @@
 library("tidyverse")
-library("dplyr")
 
 #Read the EPL files and combine them.
 epl13 <- read.csv('EPL_13-14.csv')
@@ -25,9 +24,10 @@ problem_GD_only <- filter(problem_GD, prob_GD == TRUE)
 #Part 2 
 #Q1
 titanic_df <- read_csv(url("https://raw.githubusercontent.com/plotly/datasets/master/titanic.csv"))
+write_csv(titanic_df, "titanic_df.csv")
 
-add_survival_status <- mutate(titanic_df, survival_status = ifelse(Survived == 0, "Did not survive",
-                                                            ifelse(Survived == 1, "Survived", "no")))
+add_survival_status <- mutate(titanic_df, survival_status = ifelse(Survived == 0, "Did not survive", "Survived"))
+                                                            
 
 survival_stats <- group_by(add_survival_status, survival_status)
 summarize(survival_stats, avg_age = mean(Age, na.rm = TRUE), avg_fare = mean(Fare, na.rm = TRUE))
@@ -36,7 +36,5 @@ summarize(survival_stats, avg_age = mean(Age, na.rm = TRUE), avg_fare = mean(Far
 #percentage of pasengers who survived broken out by gender and the port they came from
 lived_df <- add_survival_status %>%
      group_by(survival_status, Sex, Embarked) %>%
-       summarize(
-         survival_percentage = n() / nrow(add_survival_status)
-            ) %>%
-      filter(survival_status == "Survived")
+     summarize(survival_percentage = n() / nrow(add_survival_status)) %>%
+     filter(survival_status == "Survived")
